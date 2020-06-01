19766ced7eb30d20af57d263eb0303c4874c84be
Mayke Alisson <maykealison@gmail.com>
2020-06-01 04:47:39 -0300

    config seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     position: `FullStack Develop`,
     description: `Membro da Equipe de TI da Empresa Arcom, apaixonado por tecnologia, podcast e um eterno aprendiz.`,
     author: `@maykealisson`,
+    siteUrl: `https://maykealisson.com.br`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
@@ -72,17 +73,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mayke Alisson`,
+        short_name: `Mayke Alisson`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
