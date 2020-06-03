0a7e405406d7419f90ed07f3f3b567d1896fddbe
Mayke Alisson <maykealison@gmail.com>
2020-06-03 07:40:30 -0300

    refactor: ajuste url

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,8 +5,7 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `https://localhost:8000${url}`
-  console.log(completeURL)
+  const completeURL = `https://maykealisson.netlify.app${url}`
 
   return (
     <S.CommentsWrapper>
